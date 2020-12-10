0cc3f9d1006a802bce4eeaa61114e252ae265aaa
marquette <sioecoris@gmail.cm>
2020-12-10 15:17:14 +0100

    Creation page d'accueil avec 2 boutons

--- app/src/main/java/com/example/hardwaremonitor/MainActivity.java
+++ app/src/main/java/com/example/hardwaremonitor/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.hardwaremonitor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void netWork(View v){
+//        intent = new Intent(MainActivity.this, netWork.class);
+        startActivity(intent);
+    }
+
+    public void procs(View v){
+ //       intent = new Intent(MainActivity.this, procs.class);
+        startActivity(intent);
     }
 }
