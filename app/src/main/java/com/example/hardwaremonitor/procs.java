d25fbfccd4cbb47e4490862f141ea243b6a7a023
Guguspy <62856672+Guguspy@users.noreply.github.com>
2021-01-21 11:46:23 +0100

    ajout network.java + procs.java

---
+++ app/src/main/java/com/example/hardwaremonitor/procs.java
@@ -0,0 +1,14 @@
+package com.example.hardwaremonitor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class procs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_procs);
+    }
+}
