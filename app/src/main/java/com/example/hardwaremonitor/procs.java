52accefa4c031581037bb8f3638ec09d0df179be
Maix <maixent200074@gmail.com>
2021-03-18 08:41:12 +0100

    Update Maix

--- app/src/main/java/com/example/hardwaremonitor/procs.java
+++ app/src/main/java/com/example/hardwaremonitor/procs.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.HardwarePropertiesManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -47,6 +48,7 @@ public class procs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_procs);
         Button button = (Button) findViewById(R.id.button);
+        HardwarePropertiesManager hPM;
 
         String rep;
 
